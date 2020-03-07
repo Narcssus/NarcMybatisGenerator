63262958336bc2c8dc0affaff92e9d53eaa3fa8f
Narcssus <narcssusinnook@163.com>
2020-03-08 00:32:01 +0800

    自己的MBG

--- src/main/java/com/narc/MyCommentGenerator.java
+++ src/main/java/com/narc/MyCommentGenerator.java
@@ -1,22 +1,149 @@
 package com.narc;
 
+import org.mybatis.generator.api.CommentGenerator;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
-import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
 import org.mybatis.generator.internal.util.StringUtility;
+
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * @author : Narcssus
  * @date : 2020/3/7 16:49
  */
-public class MyCommentGenerator extends DefaultCommentGenerator {
+public class MyCommentGenerator implements CommentGenerator {
+
+    private Properties myProperties;
+
+    public MyCommentGenerator() {
+        myProperties = new Properties();
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        // 获取自定义的 properties
+        this.myProperties.putAll(properties);
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        String author = myProperties.getProperty("author", "NarcMybatisGenerator");
+        String dateFormat = myProperties.getProperty("dateFormat", "yyyy-MM-dd");
+        SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat);
+
+        // 获取表注释
+        String remarks = introspectedTable.getRemarks();
+        String tableName = introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime();
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine(" * " + remarks);
+        topLevelClass.addJavaDocLine(" * " + tableName);
+        topLevelClass.addJavaDocLine(" * ");
+        topLevelClass.addJavaDocLine(" * @author " + author);
+        topLevelClass.addJavaDocLine(" * @date " + dateFormatter.format(new Date()));
+        topLevelClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addRootComment(XmlElement rootElement) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+
+    }
 
     @Override
     public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        // 获取列注释
+        String remarks = introspectedColumn.getRemarks();
+        String columnName = introspectedColumn.getActualColumnName();
         field.addJavaDocLine("/**");
-        field.addJavaDocLine("test");
-        field.addJavaDocLine("*/");
+        field.addJavaDocLine(" * " + remarks);
+        field.addJavaDocLine(" * " + columnName);
+        field.addJavaDocLine(" */");
+        //增加长度校验
+        if (introspectedColumn.isJdbcCharacterColumn()) {
+            int length = introspectedColumn.getLength();
+            field.addAnnotation("@Size(max=" + length + ")");
+        }
+        //增加非空校验
+        if(!introspectedColumn.isNullable()){
+            field.addAnnotation("@NotNull");
+        }
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
     }
 
 }
